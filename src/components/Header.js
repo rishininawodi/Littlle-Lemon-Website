968fadfce18f0d11e2e9ce7b305a632ed9d9f7c8
Rishini <nawodirishini88@gmail.com>
2024-07-13 15:07:07 +0530

    styling element excercise

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './styles.css';
 
 function Header() {
   return (
     <header>
       <img src={logo} alt="Little Lemon logo" />
+      <h1>Little Lemon</h1>
     </header>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Main() {
   return (
     <main>
-      {/* Add your header content here */}
+      <h2>Welcome to Little Lemon</h2>
+      <p>Your favorite place for delicious food.</p>
+      <img src="path/to/image.jpg" alt="Delicious food" />
     </main>
   );
 }
