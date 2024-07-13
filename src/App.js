801f1256698e100502eb717ffd44ca6891be0176
Rishini <nawodirishini88@gmail.com>
2024-07-13 14:58:53 +0530

    setting up css layout

--- src/App.js
+++ src/App.js
@@ -16,4 +16,15 @@ function App() {
   );
 }
 
+function App() {
+  return (
+    <div className="container">
+      <div className="item">Item 1</div>
+      <div className="item">Item 2</div>
+      <div className="item">Item 3</div>
+    </div>
+  );
+}
+
+
 export default App;
